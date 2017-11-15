c801c6d34e05fc39fc62776949020383f946ec64
Kevin Wang <kwang21093@gmail.com>
2017-11-15 12:59:49 -0500

    some interfaces and specs

--- parse.mli
+++ parse.mli
@@ -1,1 +1,5 @@
-(*  *)
+exception SyntaxError of string
+
+(* parse_file [file] takes an OCamTex file and parses it into a document.
+ * throws: SyntaxError if [file] cannot be parsed. *)
+val parse_file : string -> Ast.doc
