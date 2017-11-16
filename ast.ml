b0cb06107bb128343fa60987bc0ac5fcd8a2b609
Kevin Wang <kwang21093@gmail.com>
2017-11-15 21:50:38 -0500

    revised types

--- ast.ml
+++ ast.ml
@@ -1,28 +1,34 @@
 (* The type of the abstract syntax tree (AST). *)
-type section =
-  head_expr * expr list (* Foot | Config*)
-  (* need to define foot and config types *)
+type doc = head_expr option * format * expr list
 
-and head_expr =
-  | Title of string option
-  | Author of string option
-  | MetaData
+and head_expr = title option * author option * metadata option
+
+and title = string
+
+and author = string
+
+and metadata = string
+
+and format = margins * spacing
+
+and margins = int
+
+and spacing = int
 
 and expr =
-  | Cmd of cmd * format * expr list
+  | Cmd of cmd * style option
   | Var of string
-  | Math of expr list
+  | Math of string
   | Text of string
   | Comment of string
 
 and cmd =
-  | List
-  | Table
-  | Equation
-  | InlineEq
-  | ListItem
-  | Matrix
-  | Image
+  | List of expr list
+  | Table of expr list list
+  | Equation of expr
+  | InlineEq of expr
+  | ListItem of expr
+  | Matrix of expr list list
+  | Image of expr
 
-and format =
-  | Style of string
+and style = string
