38173188ade0f635e5d15f342d14201907c1f9cd
Kevin Wang <kwang21093@gmail.com>
2017-11-20 23:02:50 -0500

    remove to_pdf

--- to_pdf.ml
+++
@@ -1,5 +0,0 @@
-open Command
-
-let pdflatex = ref (A"pdflatex")
-
-let write_to_pdf filename = 
--- to_pdf.mli
+++
@@ -1,4 +0,0 @@
-(* write_to_pdf [doc] takes an OCamTex document and converts it to a LaTeX
- * document. The name of the file is returned. *)
- val write_to_pdf : Ast.doc -> string
- 
