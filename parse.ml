54487e00c96bcf5434d85434522476098d5d8cf7
Divyansh Garg <dg595@cornell.edu>
2017-11-27 13:13:45 -0500

    Merge

--- parse.ml
+++ parse.ml
@@ -33,32 +33,22 @@ let string_of_file filename =
  IO.read_all input
 
 let parse_file filename =
-<<<<<<< HEAD
   let inx = In_channel.create filename in
   let lexbuf = from_channel inx in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
   try
     parser_start Lexer.token lexbuf;
     In_channel.close inx
-=======
-  (* let input_string = string_of_file filename in
-    let lexbuf = from_string input_string in *)
-  ([], [Text "This is text.";
+ (*  ([], [Text "This is text.";
         Comment "This is a comment.";
         Math "This is math.";
         Cmd (List ([Text "Text item 1";
                     Math "Math item 2";
                     Cmd (List ([Text "Nested text item 1";
-                                Math "Nested math item 2"], None))], None))])
-(*
-  try Parser.parse_expression Lexer.token lexbuf
->>>>>>> a4d25047815d55720d2d3ed3bd58fd1b78fc1162
+                                Math "Nested math item 2"], None))], None))]) *)
   with
       | Parser.Error | Lexer.Error -> parse_error lexbuf
       | Failure s -> unexpected_error s lexbuf
-    (* ([], [Text "This is text.";
-        Comment "This is a comment.";
-        Math "This is math."]) *)
 
 let parse_expr =
   parse Parser.parse_expression
