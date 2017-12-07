b41a951a78912fec319cb51f639223f0d42056b2
kxw2 <kxw2@cornell.edu>
2017-12-06 20:35:23 -0500

    some test cases and cleanup

--- test.ml
+++ test.ml
@@ -8,44 +8,16 @@ let strip = global_replace (regexp "\n") ""
 let equal s1 s2 = assert_equal (strip s1) (strip s2)
 
 let to_tex = [
+  "string" >:: (fun _ -> equal "string" (expr_to_tex (String "string")));
   "text" >:: (fun _ -> equal "text" (expr_to_tex (Text [String "text"])));
+  "math" >:: (fun _ -> equal "$math$" (expr_to_tex (Math (MathStr "math"))));
   "comment" >:: (fun _ -> equal "\\begin{comment}comment\\end{comment}"
                     (expr_to_tex (Comment "comment")));
-  "math" >:: (fun _ -> equal "$math$" (expr_to_tex (Math [String "math"])));
-  "unordered" >:: (fun _ -> equal "\\begin{itemize}
-\\item Text item 1
-\\item $Math item 2$
-\\begin{itemize}
-\\item Nested text item 1
-\\item $Nested math item 2$
-\\end{itemize}
-\\end{itemize}" (expr_to_tex (Cmd ("list",
-                                   None, [
-                                     Text [String "Text item 1"];
-                                     Math [String "Math item 2"];
-                                     Cmd ("list",
-                                          None, [
-                                            Text [String "Nested text item 1"];
-                                            Math [String "Nested math item 2"]
-                                          ])
-                                   ]))));
-  "ordered" >:: (fun _ -> equal "\\begin{enumerate}[label=(\\alph*)]
-\\item Text item 1
-\\item $Math item 2$
-\\begin{enumerate}[label=(\\Alph*)]
-\\item Nested text item 1
-\\item $Nested math item 2$
-\\end{enumerate}
-\\end{enumerate}" (expr_to_tex (Cmd ("list",
-                                     Some "(\\alph*)", [
-                                       Text [String "Text item 1"];
-                                       Math [String "Math item 2"];
-                                       Cmd ("list",
-                                            Some "(\\Alph*)", [
-                                              Text [String "Nested text item 1"];
-                                              Math [String "Nested math item 2"]
-                                            ])
-                                     ]))))
+  "title" >:: (fun _ -> equal "\\title{title}" (head_to_tex (Title "title")));
+  "author" >:: (fun _ -> equal "\\author{author}" (head_to_tex (Author "author")));
+  "font" >:: (fun _ -> equal ("\\usepackage[T1]{fontenc}\\usepackage{font}") (head_to_tex (Font "font")));
+  "hcomment" >:: (fun _ -> equal "\\begin{comment}comment\\end{comment}" (head_to_tex (HComment "comment")));
+  "hstring" >:: (fun _ -> equal "string" (head_to_tex (HString "string")));
 ]
 
 let suite =
