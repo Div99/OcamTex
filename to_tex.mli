9051245b7cb6dbd15ee2161dec7f67eb8d703b23
lauralindy <laura.f.lin@gmail.com>
2017-12-07 14:10:36 -0500

    to_tex comments

--- to_tex.mli
+++ to_tex.mli
@@ -2,6 +2,11 @@
  * document. The name of the file is returned. *)
 val write_to_tex : Ast.doc -> string -> string
 
+(* expr_to_tex [expr] takes an expr (the body) and converts it into Latex. The
+*  body of the Latex file as a string is returned. *)
 val expr_to_tex : Ast.expr -> string
 
+(* head_to_tex [head_expr] takes an head_expr and converts it to a Latex head
+*  description. This includes packages used and layout specifics. The string of
+* the Latex snippet is returned. *)
 val head_to_tex : Ast.head_expr -> string
